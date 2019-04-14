098a63cf1167379a0f212ffdbd3a034dae0448f3
Tyler Gene Harrison <t.g.harrison.nz@gmail.com>
2019-04-14 19:14:00 +0200

    current transducers have correction values

--- sensors.c
+++ sensors.c
@@ -34,13 +34,14 @@ void handle_current_sensor(volatile float *f32_current, uint16_t u16_ADC_reg, ui
 	if (u8_sensor_num)
 	{//batt
 		//f_new_current = (f_new_current+CORRECTION_OFFSET_BAT);// correction of offset
-		f_new_current = (f_new_current);
+		f_new_current = (f_new_current + (float)0.3);
 	}else{
 		//f_new_current = (f_new_current+CORRECTION_OFFSET_MOT);// correction of offset
-		f_new_current = (f_new_current);
+		f_new_current = (f_new_current + (float)0.3);
 	}
+	*f32_current = f_new_current;
 	
-	*f32_current = (*f32_current)*(1-LOWPASS_CONSTANT) + LOWPASS_CONSTANT*f_new_current ;// low pass filter ---------------------TODO test
+	//*f32_current = (*f32_current)*(1-LOWPASS_CONSTANT) + LOWPASS_CONSTANT*f_new_current ;// low pass filter ---------------------TODO test
 }
 
 void handle_temp_sensor(volatile uint8_t *u8_temp, uint16_t u16_ADC_reg)
